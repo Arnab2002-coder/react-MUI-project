ca621ba6dcad2f951e1591b166542f606b4edbf5
Arnab2002-coder <arnabchakraborty8128@gmail.com>
2025-07-25 16:51:17 +0530

    initial validation of Login.jsx using useState

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import {
   Box,
   Button,
@@ -12,10 +12,59 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    setError((prevErrors)=>({
+      ...prevErrors,
+      [name]: "",
+    }));
+  };
+
+  //Validating input values
+    const validate = () => {
+      const newError = {};
+
+      if (!formData.email.trim()) {
+        newError.email = "Email is required";
+      } else if (!formData.email.includes("@") || !formData.email.includes(".")){
+        newError.email = "Email is invalid";
+      }
+
+      if (!formData.password.trim()){
+        newError.password = "Password is required";
+      }else if(formData.password.length < 6) {
+        newError.password = "Password must be at least 6 characters"
+      }
+      setError(newError);
+
+      console.log("Validation errors:", newError);
+      
+      return Object.keys(newError).length === 0;
+      
+    }
+
+    const handleSubmit = (e) => {
     e.preventDefault();
-    // Auth logic here
-    navigate("/admin/list");
+
+    if (validate()) {
+      alert("Login successful!");
+      navigate("/admin/list");
+    } else {
+      alert("Please fix the errors before submitting.");
+    }
   };
 
   return (
@@ -39,6 +88,11 @@ const Login = () => {
             fullWidth
             required
             margin="normal"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            error={!!error.email}
+            helperText={error.email}
           />
           <TextField
             label="Password"
@@ -46,6 +100,11 @@ const Login = () => {
             fullWidth
             required
             margin="normal"
+            name="password"
+             value={formData.password}
+            onChange={handleChange}
+            error={!!error.password}
+            helperText={error.password}
           />
           <Button type="submit" variant="contained" fullWidth sx={{ mt: 2 }}>
             Login
